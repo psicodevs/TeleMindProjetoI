e02c5fc7540146b83fbf0259809a56c44659b10a
psicodevs <psicodevs@gmail.com>
2024-05-25 20:24:00 -0300

    Refatoração

--- index.php
+++ index.php
@@ -6,72 +6,14 @@
                 <div class="line"></div>
                 <h1>Afinal o que é o TeleMind?</h1>
                 <p>
-                    Acreditamos que todos somos responsáveis pela educação inclusiva,
-                    para que num futuro próximo tenhamos mais oportunidades 
-                    e menos desigualdade. O intuito desse projeto é criar programas 
-                    específicos de capacitação para mulheres na tecnologia, onde suas 
-                    necessidades sejam atendidas e viabilizadas, através da Educação de Qualidade.
+                    O intuito desse projeto é facilitar a administração e agendamento de consultas 
+                    para os paciente de uma cliníca de Psicologia. Que as necessidades tanto da 
+                    psicologa quanto dos pacientes sejam atendidas de maneira prática e com fácil
+                    acesso através do aplicativo TeleMind. 
                 </p>
-                <button class="btn">Conecte-se</button>
+                <button class="btn">Cadastre-se</button>
             </div>
-            <div class="banner__img">
-                <picture>
-                    <source media="(max-width:800px)" srcset="images/background.jfif">
-                    <img src="images/background.jfif">
-                </picture>
-            </div>
-        </div>
-        <img src="images/bg-pattern-intro-left-desktop.svg" class="pattern left" alt="">
-        <img src="images/bg-pattern-intro-right-desktop.svg" class="pattern right" alt="">
-    </section>
-    <!-- end banner -->
-    <section class="diferent">
-        <div class="container">
-            <div class="line"></div>
-            <br>
-            <h3 class="h3">Curiosidades</h3>
-            <div class="cards">
-                <!-- card item 0 -->
-                <div class="card-item">
-                    <img src="images/icon-snappy-process.svg" alt="">
-                    <p class="titulo">Transição de Carreira</p>
-                    <p class="description">Uma pesquisa realizada pelo RH EDC Group revelou que 53% dos brasileiros 
-                        desejam mudar de área de atuação. E a área de Tecnologia da Informação (TI) é a mais almejada por esses profissionais.</p>
-                </div>
-                <!-- card item 0 -->
-                <!-- card item 1 -->
-                <div class="card-item">
-                    <img src="images/icon-affordable-prices.svg" alt="">
-                    <p class="titulo">Mercado de Trabalho</p>
-                    <p class="description">As mulheres ainda são sub-representadas: apenas 16% dos entrevistados que se identificaram como mulheres 
-                        trabalhavam em funções técnicas de TI, enquanto 28% dos homens entrevistados.</p>
-                </div>
-                <!-- card item 1 -->
-                <!-- card item 3 -->
-                <div class="card-item">
-                    <img src="images/icon-people-first.svg" alt="">
-                    <p class="titulo">Representatividade</p>
-                    <p class="description"> A diversidade é importante para os profissionais de tecnologia: 80% dos entrevistados disseram que a diversidade 
-                        é importante para eles ao escolher uma empresa para trabalhar.</p>
-                </div>
-                <!-- card item 3 -->
-            </div>
-        </div>
-    </section>
-    <!-- end diferent -->
-    <!-- section find -->
-    <section class="find">
-        <div class="container">
-            <div class="find-background">
-                <h2 class="h2">"As pessoas são alérgicas a mudanças. Elas adoram dizer: <br>'Nós sempre fizemos assim.'
-                    Eu tento lutar por isso. <br>É por isso que eu tenho um relógio na minha parede<br>
-                    que corre ao contrário." <br> 
-                    <br>Grace Hopper</h2>
-                <a href="#" style="margin-left: auto;position: relative;z-index:9;" class="btn">Saiba mais</a>
-                <img src="images/bg-pattern-how-we-work-desktop.svg" class="pattern-find" alt="">
-            </div>
-    </section>
-    <!-- end section find -->
+            
    
     
 
