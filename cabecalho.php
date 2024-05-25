29a53764a85c45715014039c153d4f5386cdac73
psicodevs <psicodevs@gmail.com>
2024-05-25 19:03:48 -0300

    Refatoração

--- cabecalho.php
+++ cabecalho.php
@@ -5,19 +5,20 @@ if(!isset($_SESSION)) {
 ?>
 
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"/>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-    <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet"/>
-    
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="script.js">
-    <title>TeleMind</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap" rel="stylesheet">
+
+  <link rel="stylesheet" href="./style.css" />
+
+  <title>TeleMind</title>
 </head>
 
 <body>
@@ -25,13 +26,12 @@ if(!isset($_SESSION)) {
     <nav>
         <div class="container">
             <div class="logo">
-                <h2>MulheresEmTech30+</h2>
+                <h2>TeleMind</h2>
             </div>
             <ul>
                 <a href="index.php">Home</a>
-                <a href="historia.php">História</a>
-                <a href="carreira.php">Carreira</a>
-                <a href="trilhas.php">Trilhas_Tech</a>
+                <a href="cadastro.php">Cadastro</a>
+                <a href="agendar.php">Agendar</a>
                 <?php if(isset($_SESSION["autorizacao"]) && isset($_SESSION["user_name"])) {?>
                     <a href="logout.php">Logout</a>
                 <?php } else { ?>
---
+++ index.php
@@ -0,0 +1,78 @@
+<?php include('cabecalho.php'); ?>
+<!-- banner -->
+<section class="banner">
+        <div class="container">
+            <div class="banner__text">
+                <div class="line"></div>
+                <h1>Afinal o que é o TeleMind?</h1>
+                <p>
+                    Acreditamos que todos somos responsáveis pela educação inclusiva,
+                    para que num futuro próximo tenhamos mais oportunidades 
+                    e menos desigualdade. O intuito desse projeto é criar programas 
+                    específicos de capacitação para mulheres na tecnologia, onde suas 
+                    necessidades sejam atendidas e viabilizadas, através da Educação de Qualidade.
+                </p>
+                <button class="btn">Conecte-se</button>
+            </div>
+            <div class="banner__img">
+                <picture>
+                    <source media="(max-width:800px)" srcset="images/background.jfif">
+                    <img src="images/background.jfif">
+                </picture>
+            </div>
+        </div>
+        <img src="images/bg-pattern-intro-left-desktop.svg" class="pattern left" alt="">
+        <img src="images/bg-pattern-intro-right-desktop.svg" class="pattern right" alt="">
+    </section>
+    <!-- end banner -->
+    <section class="diferent">
+        <div class="container">
+            <div class="line"></div>
+            <br>
+            <h3 class="h3">Curiosidades</h3>
+            <div class="cards">
+                <!-- card item 0 -->
+                <div class="card-item">
+                    <img src="images/icon-snappy-process.svg" alt="">
+                    <p class="titulo">Transição de Carreira</p>
+                    <p class="description">Uma pesquisa realizada pelo RH EDC Group revelou que 53% dos brasileiros 
+                        desejam mudar de área de atuação. E a área de Tecnologia da Informação (TI) é a mais almejada por esses profissionais.</p>
+                </div>
+                <!-- card item 0 -->
+                <!-- card item 1 -->
+                <div class="card-item">
+                    <img src="images/icon-affordable-prices.svg" alt="">
+                    <p class="titulo">Mercado de Trabalho</p>
+                    <p class="description">As mulheres ainda são sub-representadas: apenas 16% dos entrevistados que se identificaram como mulheres 
+                        trabalhavam em funções técnicas de TI, enquanto 28% dos homens entrevistados.</p>
+                </div>
+                <!-- card item 1 -->
+                <!-- card item 3 -->
+                <div class="card-item">
+                    <img src="images/icon-people-first.svg" alt="">
+                    <p class="titulo">Representatividade</p>
+                    <p class="description"> A diversidade é importante para os profissionais de tecnologia: 80% dos entrevistados disseram que a diversidade 
+                        é importante para eles ao escolher uma empresa para trabalhar.</p>
+                </div>
+                <!-- card item 3 -->
+            </div>
+        </div>
+    </section>
+    <!-- end diferent -->
+    <!-- section find -->
+    <section class="find">
+        <div class="container">
+            <div class="find-background">
+                <h2 class="h2">"As pessoas são alérgicas a mudanças. Elas adoram dizer: <br>'Nós sempre fizemos assim.'
+                    Eu tento lutar por isso. <br>É por isso que eu tenho um relógio na minha parede<br>
+                    que corre ao contrário." <br> 
+                    <br>Grace Hopper</h2>
+                <a href="#" style="margin-left: auto;position: relative;z-index:9;" class="btn">Saiba mais</a>
+                <img src="images/bg-pattern-how-we-work-desktop.svg" class="pattern-find" alt="">
+            </div>
+    </section>
+    <!-- end section find -->
+   
+    
+
+<?php include('rodape.php'); ?>
