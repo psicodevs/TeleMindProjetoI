51329b845071495e4460164eb229c9c317933df4
psicodevs <psicodevs@gmail.com>
2024-05-26 13:24:17 -0300

    Conexão

--- conexao.php
+++ conexao.php
@@ -2,9 +2,9 @@
 
     //dados para acesso ao banco de dados
     $servidor = "localhost";
-    $banco = "telemind";
-    $usuario = "root";
-    $bdsenha = "";
+    $banco = "u638253483_mind";
+    $usuario = "u638253483_mind";
+    $bdsenha = "mind@@2024@@DB";
 
     $pdo = new PDO("mysql:host=$servidor;dbname=$banco",$usuario,$bdsenha);
 
